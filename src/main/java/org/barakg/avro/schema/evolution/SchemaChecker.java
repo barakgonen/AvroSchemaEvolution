955ac453466fba0c3bf0b21fc34df7ce30970291
barakg <barakgonen9@gmail.com>
2020-11-01 17:33:13 +0200

    Enums fixup

--- src/main/java/org/barakg/avro/schema/evolution/SchemaChecker.java
+++ src/main/java/org/barakg/avro/schema/evolution/SchemaChecker.java
@@ -1,5 +1,6 @@
 package org.barakg.avro.schema.evolution;
 
+import org.apache.avro.Schema;
 import org.apache.avro.reflect.AvroSchema;
 
 import java.util.List;
@@ -8,7 +9,7 @@ public class SchemaChecker {
 //    private static CompatibilityChecker checker = CompatibilityChecker.FULL_TRANSITIVE_CHECKER;
 //    private static CompatibilityChecker checker = CompatibilityChecker.BACKWARD_TRANSITIVE_CHECKER;
 
-    public static boolean isValidSchemaChange(AvroSchema newSchema, AvroSchema oldSchema) {
+    public static boolean isValidSchemaChange(Schema newSchema, Schema oldSchema) {
         if (areBothAvroSchemataAreEnums(newSchema, oldSchema))
             return validateEnumUniqeCompatability(newSchema, oldSchema);
         return
@@ -22,92 +23,89 @@ public class SchemaChecker {
         return true;
     }
 
-    private static boolean areBothAvroSchemataAreEnums(AvroSchema newSchema, AvroSchema oldSchema) {
-        return false;
-//        newSchema.
-//        return newSchema.rawSchema().getType().equals(Schema.Type.ENUM) &&
-//                oldSchema.rawSchema().getType().equals(Schema.Type.ENUM);
+    private static boolean areBothAvroSchemataAreEnums(Schema newSchema, Schema oldSchema) {
+        return newSchema.getType().equals(Schema.Type.ENUM) &&
+                oldSchema.getType().equals(Schema.Type.ENUM);
     }
 
-    private static boolean validateEnumUniqeCompatability(AvroSchema newSchema, AvroSchema oldSchema) {
-//        List<String> newSchemaEnumSymbols = newSchema.rawSchema().getEnumSymbols();
-//        List<String> oldSchemaEnumSymbols = oldSchema.rawSchema().getEnumSymbols();
-//        if (newSchemaEnumSymbols.size() > oldSchemaEnumSymbols.size()) {
-//            // Added new symbol to the symbols list, need to verify it was added in the end
-//            for (int index = 0; index < oldSchemaEnumSymbols.size(); index++) {
-//                if (!oldSchemaEnumSymbols.get(index).equals(newSchemaEnumSymbols.get(index)))
-//                    return false;
-//            }
-//        } else if (newSchemaEnumSymbols.size() < oldSchemaEnumSymbols.size()) {
-//            // Symbol has removed from the enum symbols list, need to make sure it was removed from the end
-//            for (int index = 0; index < newSchemaEnumSymbols.size(); index++) {
-//                if (!newSchemaEnumSymbols.get(index).equals(oldSchemaEnumSymbols.get(index)))
-//                    return false;
-//            }
-//        } else {
-//            // Sizes are the same, need to verify symbols are in the same order
-//            for (int index = 0; index < oldSchemaEnumSymbols.size(); index++)
-//                if (!oldSchemaEnumSymbols.get(index).equals(newSchemaEnumSymbols.get(index)))
-//                    return false;
-//        }
-//
+    private static boolean validateEnumUniqeCompatability(Schema newSchema, Schema oldSchema) {
+        List<String> newSchemaEnumSymbols = newSchema.getEnumSymbols();
+        List<String> oldSchemaEnumSymbols = oldSchema.getEnumSymbols();
+        if (newSchemaEnumSymbols.size() > oldSchemaEnumSymbols.size()) {
+            // Added new symbol to the symbols list, need to verify it was added in the end
+            for (int index = 0; index < oldSchemaEnumSymbols.size(); index++) {
+                if (!oldSchemaEnumSymbols.get(index).equals(newSchemaEnumSymbols.get(index)))
+                    return false;
+            }
+        } else if (newSchemaEnumSymbols.size() < oldSchemaEnumSymbols.size()) {
+            // Symbol has removed from the enum symbols list, need to make sure it was removed from the end
+            for (int index = 0; index < newSchemaEnumSymbols.size(); index++) {
+                if (!newSchemaEnumSymbols.get(index).equals(oldSchemaEnumSymbols.get(index)))
+                    return false;
+            }
+        } else {
+            // Sizes are the same, need to verify symbols are in the same order
+            for (int index = 0; index < oldSchemaEnumSymbols.size(); index++)
+                if (!oldSchemaEnumSymbols.get(index).equals(newSchemaEnumSymbols.get(index)))
+                    return false;
+        }
+
         return true;
     }
 
-    private static boolean isChangeBackwardsCompatible(AvroSchema newSchema, AvroSchema oldSchema) {
-//        List<Schema.Field> newSchemaFields = newSchema.rawSchema().getFields();
-//        List<Schema.Field> oldSchemaFields = oldSchema.rawSchema().getFields();
-//
-//        // Return true if nothing has changed
+    private static boolean isChangeBackwardsCompatible(Schema newSchema, Schema oldSchema) {
+        List<Schema.Field> newSchemaFields = newSchema.getFields();
+        List<Schema.Field> oldSchemaFields = oldSchema.getFields();
+
+        // Return true if nothing has changed
 //        if (oldSchema.deepEquals(newSchema))
 //            return true;
-//
-//        if (newSchemaFields.size() == oldSchemaFields.size()) {
-//            // Sizes are the same, need to check for the same order of fields, types and names
-//        } else {
-//            if (oldSchemaFields.size() < newSchemaFields.size()) {
-//                int index = 0;
-//                // iteration of all the fields from old schema
-//                for (index = 0; index < oldSchemaFields.size(); index++) {
-//                    if (!oldSchemaFields.get(index).equals(newSchemaFields.get(index)))
-//                        return false;
-//                }
-//                if (index > 0) {
-//                    for (; index < newSchemaFields.size(); index++) {
-//                        Schema.Field field = newSchemaFields.get(index);
-//                        if (field.schema().getType() == Schema.Type.UNION && field.getProp("defaultValue") != null)
-//                            return false;
-//                        else if (field.schema().getType() == Schema.Type.LONG ||
-//                                field.schema().getType() == Schema.Type.FLOAT ||
-//                                field.schema().getType() == Schema.Type.INT ||
-//                                field.schema().getType() == Schema.Type.STRING) {
-//                            if (!(field.defaultVal() != null))
-//                                return false;
-//                        }
-//                    }
-//                }
-//                return true;
-//                // Field added to the new schema
-//                // if (new field is nullable, return true)
-//            } else if (oldSchemaFields.size() > newSchemaFields.size()) {
-//                int index = 0;
-//                // iteration of all the fields from old schema
-//                for (index = 0; index < newSchemaFields.size(); index++) {
-//                    if (!newSchemaFields.get(index).equals(oldSchemaFields.get(index)))
-//                        return false;
-//                }
-//                if (index > 0) {
-//                    for (; index < oldSchemaFields.size(); index++) {
-//                        if (oldSchemaFields.get(index).getProp("defaultValue") != null ||
-//                                oldSchemaFields.get(index).schema().getType() != Schema.Type.UNION)
-//                            return false;
-//                    }
-//                }
-//                return true;
-//            }
-//        }
-//
-//        return false;
-        return true;
+
+        if (newSchemaFields.size() == oldSchemaFields.size()) {
+            // Sizes are the same, need to check for the same order of fields, types and names
+        } else {
+            if (oldSchemaFields.size() < newSchemaFields.size()) {
+                int index = 0;
+                // iteration of all the fields from old schema
+                for (index = 0; index < oldSchemaFields.size(); index++) {
+                    if (!oldSchemaFields.get(index).equals(newSchemaFields.get(index)))
+                        return false;
+                }
+                if (index > 0) {
+                    for (; index < newSchemaFields.size(); index++) {
+                        Schema.Field field = newSchemaFields.get(index);
+                        if (field.schema().getType() == Schema.Type.UNION && field.getProp("defaultValue") != null)
+                            return false;
+                        else if (field.schema().getType() == Schema.Type.LONG ||
+                                field.schema().getType() == Schema.Type.FLOAT ||
+                                field.schema().getType() == Schema.Type.INT ||
+                                field.schema().getType() == Schema.Type.STRING) {
+                            if (!(field.defaultVal() != null))
+                                return false;
+                        }
+                    }
+                }
+                return true;
+                // Field added to the new schema
+                // if (new field is nullable, return true)
+            } else if (oldSchemaFields.size() > newSchemaFields.size()) {
+                int index = 0;
+                // iteration of all the fields from old schema
+                for (index = 0; index < newSchemaFields.size(); index++) {
+                    if (!newSchemaFields.get(index).equals(oldSchemaFields.get(index)))
+                        return false;
+                }
+                if (index > 0) {
+                    for (; index < oldSchemaFields.size(); index++) {
+                        if (oldSchemaFields.get(index).getProp("defaultValue") != null ||
+                                oldSchemaFields.get(index).schema().getType() != Schema.Type.UNION)
+                            return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- src/test/java/org/barakg/avro/schema/evolution/EnumTester.java
+++ src/test/java/org/barakg/avro/schema/evolution/EnumTester.java
@@ -1,6 +1,6 @@
 package org.barakg.avro.schema.evolution;
 
-import org.apache.avro.reflect.AvroSchema;
+import org.apache.avro.Schema;
 import org.barakg.avro.schema.evolution.utils.FileReadingUtils;
 import org.junit.Test;
 
@@ -19,216 +19,216 @@ public class EnumTester {
             "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
             "}";
 
-//    public static final AvroSchema baseEnum = AvroUtils.parseSchema(enumBaseDefinition);
-//
-//    @Test
-//    public void testReadEnumFromFile() {
-//        String enumSchemaAsString = FileReadingUtils.getFileContent("enums/MyEnum.avsc", EnumTester.class);
-//        assertEquals("enums not the same", enumBaseDefinition, enumSchemaAsString);
-//    }
-//
-//    @Test
-//    public void testRemovingEnumValueFromTheBeginingOfEnumSymbolsBreakSchemaCompatability() {
-//        final String enumBaseDefinitionWithFirstSymbolRemoved = "{" +
-//                "\"name\":\"SystemName\"," +
-//                "\"type\":\"enum\"," +
-//                "\"symbols\":[" +
-//                "\"A\"," +
-//                "\"B\"," +
-//                "\"C\"" +
-//                "]," +
-//                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
-//                "}";
-//        AvroSchema modifiedEnum = AvroUtils.parseSchema(enumBaseDefinitionWithFirstSymbolRemoved);
-//        assertFalse("enums are the same", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnum));
-//    }
-//
-//    @Test
-//    public void testRemovingEnumValueFromTheMiddleOfEnumSymbolsBreakSchemaCompatability() {
-//        String enumBaseDefinitionWithMiddleSymbolRemoved = "{" +
-//                "\"name\":\"SystemName\"," +
-//                "\"type\":\"enum\"," +
-//                "\"symbols\":[" +
-//                "\"NONE\"," +
-//                "\"B\"," +
-//                "\"C\"" +
-//                "]," +
-//                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
-//                "}";
-//        AvroSchema modifiedEnum = AvroUtils.parseSchema(enumBaseDefinitionWithMiddleSymbolRemoved);
-//        assertFalse("enums are the same", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnum));
-//    }
-//
-//    @Test
-//    public void testRemovingEnumValueFromTheEndOfEnumSymbolsBreakSchemaCompatability() {
-//        // TODO: need to decide weather it should break the build..
-//        String enumBaseDefinitionWithLastSymbolRemoved = "{" +
-//                "\"name\":\"SystemName\"," +
-//                "\"type\":\"enum\"," +
-//                "\"symbols\":[" +
-//                "\"NONE\"," +
-//                "\"A\"," +
-//                "\"B\"" +
-//                "]," +
-//                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
-//                "}";
-//        AvroSchema modifiedEnum = AvroUtils.parseSchema(enumBaseDefinitionWithLastSymbolRemoved);
-//        assertTrue("enums are the same", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnum));
-//    }
-//
-//    @Test
-//    public void testAddingEnumValueAtTheBeginning() {
-//        String enumBaseDefinitionWithSymbolAddedBegin = "{" +
-//                "\"name\":\"SystemName\"," +
-//                "\"type\":\"enum\"," +
-//                "\"symbols\":[" +
-//                "\"TEST\"," +
-//                "\"NONE\"," +
-//                "\"A\"," +
-//                "\"B\"," +
-//                "\"C\"" +
-//                "]," +
-//                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
-//                "}";
-//        AvroSchema modifiedEnum = AvroUtils.parseSchema(enumBaseDefinitionWithSymbolAddedBegin);
-//        assertFalse("", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnum));
-//    }
-//
-//    @Test
-//    public void testAddingEnumValueAtTheMiddle() {
-//        String enumBaseDefinitionWithSymbolAddedMiddle = "{" +
-//                "\"name\":\"SystemName\"," +
-//                "\"type\":\"enum\"," +
-//                "\"symbols\":[" +
-//                "\"NONE\"," +
-//                "\"A\"," +
-//                "\"B\"," +
-//                "\"b\"," +
-//                "\"C\"" +
-//                "]," +
-//                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
-//                "}";
-//        AvroSchema modifiedEnum = AvroUtils.parseSchema(enumBaseDefinitionWithSymbolAddedMiddle);
-//        assertFalse("", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnum));
-//    }
-//
-//    @Test
-//    public void testAddingEnumValueAtTheEnd() {
-//        String enumBaseDefinitionWithSymbolAddedEnd = "{" +
-//                "\"name\":\"SystemName\"," +
-//                "\"type\":\"enum\"," +
-//                "\"symbols\":[" +
-//                "\"NONE\"," +
-//                "\"A\"," +
-//                "\"B\"," +
-//                "\"C\"," +
-//                "\"D\"" +
-//                "]," +
-//                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
-//                "}";
-//        AvroSchema modifiedEnum = AvroUtils.parseSchema(enumBaseDefinitionWithSymbolAddedEnd);
-//        assertTrue("", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnum));
-//    }
-//
-//    @Test
-//    public void testReorderingBegin() {
-//        String enumBaseDefinitionWithSymbolReorderedBegin = "{" +
-//                "\"name\":\"SystemName\"," +
-//                "\"type\":\"enum\"," +
-//                "\"symbols\":[" +
-//                "\"A\"," +
-//                "\"NONE\"," +
-//                "\"B\"," +
-//                "\"C\"" +
-//                "]," +
-//                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
-//                "}";
-//        AvroSchema modifiedEnum = AvroUtils.parseSchema(enumBaseDefinitionWithSymbolReorderedBegin);
-//        assertFalse("", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnum));
-//    }
-//
-//    @Test
-//    public void testReorderingMiddle() {
-//        String enumBaseDefinitionWithSymbolReorderedMiddle = "{" +
-//                "\"name\":\"SystemName\"," +
-//                "\"type\":\"enum\"," +
-//                "\"symbols\":[" +
-//                "\"NONE\"," +
-//                "\"B\"," +
-//                "\"A\"," +
-//                "\"C\"" +
-//                "]," +
-//                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
-//                "}";
-//        AvroSchema modifiedEnum = AvroUtils.parseSchema(enumBaseDefinitionWithSymbolReorderedMiddle);
-//        assertFalse("", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnum));
-//    }
-//
-//    @Test
-//    public void testReorderingEnd() {
-//        String enumBaseDefinitionWithSymbolReorderedEnd = "{" +
-//                "\"name\":\"SystemName\"," +
-//                "\"type\":\"enum\"," +
-//                "\"symbols\":[" +
-//                "\"NONE\"," +
-//                "\"A\"," +
-//                "\"C\"," +
-//                "\"B\"" +
-//                "]," +
-//                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
-//                "}";
-//        AvroSchema modifiedEnum = AvroUtils.parseSchema(enumBaseDefinitionWithSymbolReorderedEnd);
-//        assertFalse("", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnum));
-//    }
-//
-//    @Test
-//    public void testRenameBegin() {
-//        String enumBaseDefinitionWithRenamingBegin = "{" +
-//                "\"name\":\"SystemName\"," +
-//                "\"type\":\"enum\"," +
-//                "\"symbols\":[" +
-//                "\"NoNE\"," +
-//                "\"A\"," +
-//                "\"B\"," +
-//                "\"C\"" +
-//                "]," +
-//                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
-//                "}";
-//        AvroSchema modifiedEnum = AvroUtils.parseSchema(enumBaseDefinitionWithRenamingBegin);
-//        assertFalse("", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnum));
-//    }
-//
-//    @Test
-//    public void testRenameMiddle() {
-//        final String enumBaseDefinitionWithFieldRenamingMiddle = "{" +
-//                "\"name\":\"SystemName\"," +
-//                "\"type\":\"enum\"," +
-//                "\"symbols\":[" +
-//                "\"NONE\"," +
-//                "\"A\"," +
-//                "\"b\"," +
-//                "\"C\"" +
-//                "]," +
-//                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
-//                "}";
-//        AvroSchema modifiedEnum = AvroUtils.parseSchema(enumBaseDefinitionWithFieldRenamingMiddle);
-//        assertFalse("", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnum));
-//    }
-//
-//    @Test
-//    public void testRenameEnd() {
-//        final String enumBaseDefinitionWithFieldRenamingEnd = "{" +
-//                "\"name\":\"SystemName\"," +
-//                "\"type\":\"enum\"," +
-//                "\"symbols\":[" +
-//                "\"NONE\"," +
-//                "\"A\"," +
-//                "\"B\"," +
-//                "\"c\"" +
-//                "]," +
-//                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
-//                "}";
-//        AvroSchema modifiedEnum = AvroUtils.parseSchema(enumBaseDefinitionWithFieldRenamingEnd);
-//        assertFalse("", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnum));
-//    }
+    private static Schema baseEnumDefinitionSchema = Schema.parse(enumBaseDefinition);
+
+    @Test
+    public void testReadEnumFromFile() {
+        String enumSchemaAsString = FileReadingUtils.getFileContent("enums/MyEnum.avsc", EnumTester.class);
+        assertEquals("enums not the same", enumBaseDefinition, enumSchemaAsString);
+    }
+
+    @Test
+    public void testRemovingEnumValueFromTheBeginingOfEnumSymbolsBreakSchemaCompatability() {
+        final String enumBaseDefinitionWithFirstSymbolRemoved = "{" +
+                "\"name\":\"SystemName\"," +
+                "\"type\":\"enum\"," +
+                "\"symbols\":[" +
+                "\"A\"," +
+                "\"B\"," +
+                "\"C\"" +
+                "]," +
+                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
+                "}";
+        Schema modifiedEnum = Schema.parse(enumBaseDefinitionWithFirstSymbolRemoved);
+        assertFalse("enums are the same", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnumDefinitionSchema));
+    }
+
+    @Test
+    public void testRemovingEnumValueFromTheMiddleOfEnumSymbolsBreakSchemaCompatability() {
+        String enumBaseDefinitionWithMiddleSymbolRemoved = "{" +
+                "\"name\":\"SystemName\"," +
+                "\"type\":\"enum\"," +
+                "\"symbols\":[" +
+                "\"NONE\"," +
+                "\"B\"," +
+                "\"C\"" +
+                "]," +
+                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
+                "}";
+        Schema modifiedEnum = Schema.parse(enumBaseDefinitionWithMiddleSymbolRemoved);
+        assertFalse("enums are the same", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnumDefinitionSchema));
+    }
+
+    @Test
+    public void testRemovingEnumValueFromTheEndOfEnumSymbolsBreakSchemaCompatability() {
+        // TODO: need to decide weather it should break the build..
+        String enumBaseDefinitionWithLastSymbolRemoved = "{" +
+                "\"name\":\"SystemName\"," +
+                "\"type\":\"enum\"," +
+                "\"symbols\":[" +
+                "\"NONE\"," +
+                "\"A\"," +
+                "\"B\"" +
+                "]," +
+                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
+                "}";
+        Schema modifiedEnum = Schema.parse(enumBaseDefinitionWithLastSymbolRemoved);
+        assertTrue("enums are the same", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnumDefinitionSchema));
+    }
+
+    @Test
+    public void testAddingEnumValueAtTheBeginning() {
+        String enumBaseDefinitionWithSymbolAddedBegin = "{" +
+                "\"name\":\"SystemName\"," +
+                "\"type\":\"enum\"," +
+                "\"symbols\":[" +
+                "\"TEST\"," +
+                "\"NONE\"," +
+                "\"A\"," +
+                "\"B\"," +
+                "\"C\"" +
+                "]," +
+                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
+                "}";
+        Schema modifiedEnum = Schema.parse(enumBaseDefinitionWithSymbolAddedBegin);
+        assertFalse("", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnumDefinitionSchema));
+    }
+
+    @Test
+    public void testAddingEnumValueAtTheMiddle() {
+        String enumBaseDefinitionWithSymbolAddedMiddle = "{" +
+                "\"name\":\"SystemName\"," +
+                "\"type\":\"enum\"," +
+                "\"symbols\":[" +
+                "\"NONE\"," +
+                "\"A\"," +
+                "\"B\"," +
+                "\"b\"," +
+                "\"C\"" +
+                "]," +
+                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
+                "}";
+        Schema modifiedEnum = Schema.parse(enumBaseDefinitionWithSymbolAddedMiddle);
+        assertFalse("", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnumDefinitionSchema));
+    }
+
+    @Test
+    public void testAddingEnumValueAtTheEnd() {
+        String enumBaseDefinitionWithSymbolAddedEnd = "{" +
+                "\"name\":\"SystemName\"," +
+                "\"type\":\"enum\"," +
+                "\"symbols\":[" +
+                "\"NONE\"," +
+                "\"A\"," +
+                "\"B\"," +
+                "\"C\"," +
+                "\"D\"" +
+                "]," +
+                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
+                "}";
+        Schema modifiedEnum = Schema.parse(enumBaseDefinitionWithSymbolAddedEnd);
+        assertTrue("", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnumDefinitionSchema));
+    }
+
+    @Test
+    public void testReorderingBegin() {
+        String enumBaseDefinitionWithSymbolReorderedBegin = "{" +
+                "\"name\":\"SystemName\"," +
+                "\"type\":\"enum\"," +
+                "\"symbols\":[" +
+                "\"A\"," +
+                "\"NONE\"," +
+                "\"B\"," +
+                "\"C\"" +
+                "]," +
+                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
+                "}";
+        Schema modifiedEnum = Schema.parse(enumBaseDefinitionWithSymbolReorderedBegin);
+        assertFalse("", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnumDefinitionSchema));
+    }
+
+    @Test
+    public void testReorderingMiddle() {
+        String enumBaseDefinitionWithSymbolReorderedMiddle = "{" +
+                "\"name\":\"SystemName\"," +
+                "\"type\":\"enum\"," +
+                "\"symbols\":[" +
+                "\"NONE\"," +
+                "\"B\"," +
+                "\"A\"," +
+                "\"C\"" +
+                "]," +
+                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
+                "}";
+        Schema modifiedEnum = Schema.parse(enumBaseDefinitionWithSymbolReorderedMiddle);
+        assertFalse("", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnumDefinitionSchema));
+    }
+
+    @Test
+    public void testReorderingEnd() {
+        String enumBaseDefinitionWithSymbolReorderedEnd = "{" +
+                "\"name\":\"SystemName\"," +
+                "\"type\":\"enum\"," +
+                "\"symbols\":[" +
+                "\"NONE\"," +
+                "\"A\"," +
+                "\"C\"," +
+                "\"B\"" +
+                "]," +
+                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
+                "}";
+        Schema modifiedEnum = Schema.parse(enumBaseDefinitionWithSymbolReorderedEnd);
+        assertFalse("", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnumDefinitionSchema));
+    }
+
+    @Test
+    public void testRenameBegin() {
+        String enumBaseDefinitionWithRenamingBegin = "{" +
+                "\"name\":\"SystemName\"," +
+                "\"type\":\"enum\"," +
+                "\"symbols\":[" +
+                "\"NoNE\"," +
+                "\"A\"," +
+                "\"B\"," +
+                "\"C\"" +
+                "]," +
+                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
+                "}";
+        Schema modifiedEnum = Schema.parse(enumBaseDefinitionWithRenamingBegin);
+        assertFalse("", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnumDefinitionSchema));
+    }
+
+    @Test
+    public void testRenameMiddle() {
+        final String enumBaseDefinitionWithFieldRenamingMiddle = "{" +
+                "\"name\":\"SystemName\"," +
+                "\"type\":\"enum\"," +
+                "\"symbols\":[" +
+                "\"NONE\"," +
+                "\"A\"," +
+                "\"b\"," +
+                "\"C\"" +
+                "]," +
+                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
+                "}";
+        Schema modifiedEnum = Schema.parse(enumBaseDefinitionWithFieldRenamingMiddle);
+        assertFalse("", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnumDefinitionSchema));
+    }
+
+    @Test
+    public void testRenameEnd() {
+        final String enumBaseDefinitionWithFieldRenamingEnd = "{" +
+                "\"name\":\"SystemName\"," +
+                "\"type\":\"enum\"," +
+                "\"symbols\":[" +
+                "\"NONE\"," +
+                "\"A\"," +
+                "\"B\"," +
+                "\"c\"" +
+                "]," +
+                "\"namespace\":\"org.mashov.bla.blu.bly.enums\"" +
+                "}";
+        Schema modifiedEnum = Schema.parse(enumBaseDefinitionWithFieldRenamingEnd);
+        assertFalse("", SchemaChecker.isValidSchemaChange(modifiedEnum, baseEnumDefinitionSchema));
+    }
 }
